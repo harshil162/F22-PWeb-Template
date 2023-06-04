17c0b506cffe89a1df2d09213885696acf0f9a1d
Harshil Pahuja <71945852+harshil162@users.noreply.github.com>
2023-06-04 19:59:11 -0400

    Update index.js

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -4,7 +4,7 @@ import {useNavigate} from 'react-router-dom';
 
 import Proj1 from '../../images/GBMFinder.png';
 import Proj2 from '../../images/blackjack.jpg';
-import Proj3 from '../../images/ObjectCatcher.png';
+import Proj3 from '../../images/Dyslexifi.png';
 
 import './portfolio.css';
 
