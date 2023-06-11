fc0105f51b2f3c672d81cfffe7a785c7cb73f22d
Harshil Pahuja <71945852+harshil162@users.noreply.github.com>
2023-06-10 22:59:24 -0400

    changes

--- src/Components/Introduction/index.js
+++ src/Components/Introduction/index.js
@@ -11,7 +11,7 @@ const Introduction = () => {
                     <img class="self_image" src={ProfilePic}/>
                     <div class="intro__section">
                         <h1 class="intro__title">Hi, my name is Harshil Pahuja.</h1>
-                        <p class="intro__subtitle">I am currently a freshman at the University of Florida.</p>
+                        <p class="intro__subtitle">I am currently a sophomore at the University of Florida.</p>
                         <p class="intro__memo">I am currently majoring in Computer Science. I describe myself
                         as hardworking and intrinsically driven. My interests within Computer Science 
                         include artificial intelligence, human-computer interaction, and web development. </p>
